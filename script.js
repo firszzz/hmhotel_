5680039a7fec8349da6c058ed31eca30268f8b8c
Andrey <n1an27f@gmail.com>
2022-10-27 00:20:53 +1000

    made some adaptive things, also usable widget, svg logo and other text

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function init(){
                 type: "horizontal",
                 uid: "7623eac4-d00f-4de6-99f1-4190f415fae0",
                 lang: "ru",
-                width: "960px",
+                width: "100%",
                 background: "#9f64a6",
                 bg_alpha: "100",
                 padding: "20",
@@ -41,7 +41,7 @@ function init(){
                 // inp_bordhover: "#30b6bc",
                 // inp_bordcolor: "#30b6bc",
                 // border_color: "#30b6bc",
-                title: "Забронировать прямо сейчас",
+                title: "Онлайн бронирование",
                 inp_alpha: "100",
                 btn_background: "#f8f8f8",
                 btn_background_over: "#ffffff",
@@ -67,19 +67,27 @@ function init(){
                 title_color: "#ffffff",
                 title_size: "20",
                 inp_color: "#222222",
-                inp_bordhover: "#30b6bc",
-                inp_bordcolor: "#30b6bc",
+                //inp_bordhover: "#30b6bc",
+                //inp_bordcolor: "#30b6bc",
                 border: "on",
-                border_color: "#30b6bc",
-                title: "Забронировать прямо сейчас",
+                //border_color: "#30b6bc",
+                title: "Онлайн бронирование",
                 inp_alpha: "100",
                 btn_background: "#f8f8f8",
                 btn_background_over: "#ffffff",
                 btn_textcolor: "#222222",
                 btn_textover: "#222222",
-                btn_bordcolor: "#30b6bc",
+                //btn_bordcolor: "#30b6bc",
                 btn_bordhover: "#cccccc",
             });
         });
     }
 })(jQuery);
+
+let checkR = document.getElementById("checkRooms");
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+checkR.href = `https://reservationsteps.ru/rooms/index/7623eac4-d00f-4de6-99f1-4190f415fae0?lang=ru&colorSchemePreview=0&onlyrooms=&name=&surname=&email=&phone=&orderid=&servicemode=0&firstroom=0&dfrom=${day}-${month}-${year}&dto=${day + 1}-${month}-${year}&adults=1`;
